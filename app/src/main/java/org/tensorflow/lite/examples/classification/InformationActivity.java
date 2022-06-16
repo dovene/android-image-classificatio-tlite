02f5c0d68ad87b3e6635a77b0d6bbebf33626fa8
DOVENE AGOGUE <adovene@gmail.com>
2022-06-16 08:52:17 +0200

    updates

--- app/src/main/java/org/tensorflow/lite/examples/classification/InformationActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/InformationActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.Map;
@@ -21,20 +22,23 @@ import static java.util.Map.entry;
 public class InformationActivity extends AppCompatActivity {
     public static String CONTENT = "content";
     static Map<String, String> articles = Map.ofEntries(
-            entry("Adidas Original Tee Shirt A Bandes Adicolor Classi", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("tommy hilfiger Tee Shirt A Bandes RN 0562 Blanc", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune3", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune4", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune5", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune6", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
-            entry("adidas original Tee Shirt A Bandes HE9550 Jaune7", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html")
+            entry("Adidas original Tee Shirt A Bandes Adicolor Rouge", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-adicolor-classics-he9547-rouge-296457.html"),
+            entry("Adidas original Tee Shirt A Bandes HE9550 Jaune", "https://www.laboutiqueofficielle.com/achat-t-shirts/adidas-tee-shirt-a-bandes-he9550-jaune-310993.html"),
+            entry("Adidas Baskets NY 90 FZ2247 White Royal Blue", "https://www.laboutiqueofficielle.com/achat-baskets-basses/adidas-baskets-ny-90-fz2247-footwear-white-royal-blue-247735.html"),
+            entry("Calvin Klein Baskets Runner Black Bright White", "https://www.laboutiqueofficielle.com/achat-baskets-basses/calvin-klein-baskets-runner-sneaker-lace-up-0086-black-bright-white-266592.html"),
+            entry("Casquette Neon 940 12375788 Blanc Jaune Fluo", "https://www.laboutiqueofficielle.com/achat-casquettes-de-baseball/new-era-casquette-neon-940-new-york-yankees-12375788-blanc-jaune-fluo-216137.html"),
+            entry("Casquette Trucker Essential  NY 12 Camel Noir", "https://www.laboutiqueofficielle.com/achat-trucker/new-era-casquette-trucker-essential-af-new-york-yankees-12589246-camel-noir-235669.html"),
+            entry("FINAL CLUB Pantalon Cargo Skinny Fit 944 Camel", "https://www.laboutiqueofficielle.com/achat-pantalons-cargo/final-club-pantalon-cargo-skinny-fit-944-camel-304431.html"),
+            entry("Frilivin Chemise Jean A CA-639 Bleu Denim", "https://www.laboutiqueofficielle.com/achat-chemises-manches-courtes/frilivin-chemise-jean-a-manches-courtes-ca-639-bleu-denim-305633.html"),
+            entry("LBO Chemise Slim Fit 159 Carreaux Noir Rouge", "https://www.laboutiqueofficielle.com/achat-chemises-manches-longues/lbo-chemise-manches-longues-slim-fit-159-carreaux-noir-rouge-103638.html"),
+            entry("Tommy hilfiger Tee Shirt A Bandes RN 0562 Blanc", "https://www.laboutiqueofficielle.com/achat-t-shirts/tommy-hilfiger-tee-shirt-a-bandes-rn-0562-blanc-221367.html")
     );
 
 
 
-    public static Intent newIntent(Context context, String content) {
+    public static synchronized Intent newIntent(Context context, String content) {
         final Intent intent = new Intent(context, InformationActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         intent.putExtra(CONTENT, content);
         return intent;
     }
@@ -53,7 +57,17 @@ public class InformationActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openUrl(Objects.requireNonNull(articles.get(getIntent().getStringExtra(CONTENT))));
+                String label = getIntent().getStringExtra(CONTENT);
+                if (articles.get(label) != null) {
+                    openUrl(articles.get(label));
+                }
+            }
+        });
+
+        findViewById(R.id.scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               finish();
             }
         });
     }
